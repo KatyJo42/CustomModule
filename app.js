26f5977150b100ca925f4bdc977280d6526daa02
Johnston <300077526@fvtc.edu>
2025-04-09 12:57:42 -0500

    finished fortunes

---
+++ app.js
@@ -0,0 +1,7 @@
+const getFortune = require("./fortune") //in quotes- pulling in that code from another file
+
+console.log(getFortune())
+
+
+
+
---
+++ fortune.js
@@ -0,0 +1,10 @@
+//array 
+const fortunes = require("./fortunes.json")
+//function
+const getFortune = () => 
+{
+    const randomNumber = Math.floor(Math.random() * fortunes.length)
+    return fortunes[randomNumber]
+}
+
+module.exports = getFortune
